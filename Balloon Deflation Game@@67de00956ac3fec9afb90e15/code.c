fc05c5af83f4b32eda119329d3dd06a1670fa38c
Palak Sharma <palaksharma4111@gmail.com>
2025-03-28 15:54:14 +0530

    Solution to Balloon Deflation Game

---
+++ Balloon Deflation Game@@67de00956ac3fec9afb90e15/code.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    int i;
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    printf("%d",n);
+    printf("\n");
+    if(arr[i]==0){
+        n=n-1;
+    }
+    // minimum code
+    int min=arr[0];
+    for(int i=0;i<n;i++){
+        if(arr[i]>min){
+            min=arr[i];
+        }
+    }
+    arr[i]=arr[i]-min;
+    return0;
+}
